f843ff691c03c2bd58e2e68663bfd11b947f8e7e
David Young-Chan Kay <davidykay@gmail.com>
2010-05-10 12:00:33 -0400

    RegisterServlet - now working properly

--- WEB-INF/classes/RegisterServlet.java
+++ WEB-INF/classes/RegisterServlet.java
@@ -2,6 +2,8 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.http.*;
 import java.sql.*;
+import java.security.*;
+import java.math.*;
 //import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 
 public class RegisterServlet extends HttpServlet implements Servlet {
@@ -10,6 +12,7 @@ public class RegisterServlet extends HttpServlet implements Servlet {
         response.setContentType("text/html");
         java.io.PrintWriter writer = response.getWriter();
 
+        //Everything in a servlet is written out, like PHP, to the browser
         writer.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">");
         writer.println("<HTML>");
         writer.println("<HEAD>");
@@ -32,16 +35,6 @@ public class RegisterServlet extends HttpServlet implements Servlet {
         writer.println("</FORM>");
         writer.println("</BODY>");
         writer.println("</HTML>");
-
-        //Everything in a servlet is written out, like PHP, to the browser
-        //writer.println("<html>");
-        //writer.println("<head>");
-        //writer.println("<title> Register Servlet </title>");
-        //writer.println("</head>");
-        //writer.println("<body>");
-        //writer.println("Hello, world! I'm a servlet, and this time is: " + new java.util.Date().toString());
-        //writer.println("</body>");
-        //writer.println("</html>");
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) 
@@ -53,15 +46,18 @@ public class RegisterServlet extends HttpServlet implements Servlet {
         String password = request.getParameter("password");
         String email    = request.getParameter("email");
 
-        boolean success = false;
+        boolean success = true;
         try {
-            success = registerUser( userName,
+            int status = registerUser( userName,
                 password,
                 email
             );
+            if (status == -1) {
+                success = false;
+            }
         //} catch (MySQLIntegrityConstraintViolationException ex) {
         } catch (Exception ex) {
-            
+            success = false;
         }
         //Everything in a servlet is written out, like PHP, to the browser
         writer.println("<html>");
@@ -74,6 +70,7 @@ public class RegisterServlet extends HttpServlet implements Servlet {
         } else {
             writer.println("Registration Failed!");
         }
+        writer.println("<br />");
         writer.println(
             String.format(
                 "Username: %s, Password: %s, Email: %s",
@@ -82,18 +79,45 @@ public class RegisterServlet extends HttpServlet implements Servlet {
                 email
             )
         );
+
+        writer.println("<br />");
+        writer.println("<br />");
+        writer.println("<a href=\"/dk/register\">Return to registration page</a>");
         writer.println("</body>");
         writer.println("</html>");
     }
 
     /**
-     * Returns boolean indicating success
+     * TODO: Merge this with the hash function in ChatClient
      */
-    private boolean registerUser(String userName, String password, String email) 
+    private String hashPassword(String ps) {
+        System.out.println("hashPassword()");
+        MessageDigest m = null;
+        try {
+            //byte[] bytes = ps.getBytes("ASCII");
+            byte[] bytes = ps.getBytes("UTF-8");
+            m = MessageDigest.getInstance("MD5");
+            m.update(bytes, 0, bytes.length);
+        } catch (Exception ex) {
+            System.err.println("MD5 failed");
+        }
+        String md5Password = new BigInteger(1, m.digest()).toString(16);
+        System.out.println("MD5: " + md5Password);
+
+        return md5Password;
+    }
+
+    /**
+     * Hashes password before inserting
+     * Returns int indicating rows
+     */
+    private int registerUser(String userName, String password, String email) 
     //throws MySQLIntegrityConstraintViolationException {
     throws Exception {
-        boolean result = false;
+        int retVal = -1;
         Connection conn = null;
+        
+        password = hashPassword(password);
 
         try {
             String dbUser     = "ist239";
@@ -125,19 +149,7 @@ public class RegisterServlet extends HttpServlet implements Servlet {
                 email
             );
 
-            int retVal = prepStatement.executeUpdate();
-            if (retVal == 0) {
-                result = true;
-            } else {
-                System.err.println(
-                    String.format(
-                        "INSERT FAILED: Invalid user: %s. password: %s, email: %s",
-                        userName,
-                        password,
-                        email
-                    )
-                );
-            }
+            retVal = prepStatement.executeUpdate();
         }
         catch (Exception e) {
             System.err.println("Cannot connect to database server");
@@ -152,14 +164,13 @@ public class RegisterServlet extends HttpServlet implements Servlet {
                 catch (Exception e) { /* ignore close errors */ }
             }
         }
-        //return result;
-        //this was backwards
-        return !result;
+        return retVal;
     }
 
     public static void main(String[] sa) {
         try {
-            new RegisterServlet().registerUser(
+            RegisterServlet rs = new RegisterServlet();
+            rs.registerUser(
                 "bob",
                 "test",
                 "bob@yahoo.com"
